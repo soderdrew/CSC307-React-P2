72853773c8cc68e94acfcfe433f1b81117133163
Drew Soderquist <soderdrews@gmail.com>
2022-09-26 10:48:31 -0700

    Added form to input new characters

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,28 +1,13 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspiring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-    ]);  
+  const [characters, setCharacters] = useState([]);  
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        <Form />
       </div>
     )
 
