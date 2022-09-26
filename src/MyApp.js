9eb2869fa3201ca49400f088c5f9331f38511980
Drew Soderquist <soderdrews@gmail.com>
2022-09-26 10:59:05 -0700

    Finished form to be able to submit new characters

--- src/MyApp.js
+++ src/MyApp.js
@@ -7,16 +7,22 @@ function MyApp() {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
-        <Form />
+        <Form handleSubmit={updateList} />
       </div>
     )
 
-function removeOneCharacter (index) {
-  const updated = characters.filter((character, i) => {
-      return i !== index
-    });
-    setCharacters(updated);
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
   }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  
 }
 
 
