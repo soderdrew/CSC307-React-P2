35d6f1a46f73747c421f1b2b2f6e30cde0a7e60c
Drew Soderquist <soderdrews@gmail.com>
2022-09-28 11:39:07 -0700

    Added POST request

--- expressjs-backend/index.js
+++ expressjs-backend/index.js
@@ -12,6 +12,48 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });      
 
+app.get('/users', (req, res) => {
+   const name = req.query.name;
+   console.log(name)
+   if (name != undefined){
+       let result = findUserByName(name);
+       result = {users_list: result};
+       res.send(result);
+   }
+   else{
+       res.send(users);
+   }
+});
+
+app.get('/users/:id', (req, res) => {
+   const id = req.params['id']; //or req.params.id
+   let result = findUserById(id);
+   if (result === undefined || result.length == 0)
+       res.status(404).send('Resource not found.');
+   else {
+       result = {users_list: result};
+       res.send(result);
+   }
+});
+
+app.post('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.status(200).end();
+});
+
+function addUser(user){
+   users['users_list'].push(user);
+}
+
+function findUserById(id) {
+   return users['users_list'].find( (user) => user['id'] === id); // or line below
+   //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+const findUserByName = (name) => { 
+   return users['users_list'].filter( (user) => user['name'] === name); 
+}
 
 const users = { 
    users_list :
@@ -25,6 +67,11 @@ const users = {
          id : 'abc123', 
          name: 'Mac',
          job: 'Bouncer',
+      },
+      {
+         id : 'z31dad', 
+         name: 'Mac',
+         job: 'Bouncy Ball',
       },
       {
          id : 'ppp222', 
@@ -43,7 +90,3 @@ const users = {
       }
    ]
 }
-
-app.get('/users', (req, res) => {
-   res.send(users);
-})
