b971fd2317e41ca6b1220b5d21d9837512c814b4
Drew Soderquist <soderdrews@gmail.com>
2022-09-30 12:45:58 -0700

    Added get search by name and job

--- expressjs-backend/index.js
+++ expressjs-backend/index.js
@@ -12,13 +12,26 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });      
 
+// http://localhost:5000/users?name=Mac&job=bartender
 app.get('/users', (req, res) => {
    const name = req.query.name;
-   console.log(name)
+   const job = req.query.job;
+   console.log(name);
+   console.log(job);
    if (name != undefined){
-       let result = findUserByName(name);
-       result = {users_list: result};
-       res.send(result);
+      let result = findUserByName(name);
+      console.log(result)
+      if (job != undefined){
+         // pass in a list     
+         let finalResult = findUserByJob(job, result);
+         res.send(finalResult);
+       }
+       else {
+         //let result = findUserByName(name);
+         result = {users_list: result};
+         res.send(result);
+       }
+       
    }
    else{
        res.send(users);
@@ -42,6 +55,19 @@ app.post('/users', (req, res) => {
    res.status(200).end();
 });
 
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id']; //or req.params.id
+   let result = findUserById(id);
+   //console.log(result);
+   if (result === undefined || result.length == 0)
+       res.status(404).send('Resource not found.');
+   else {
+       users.users_list.splice(users.users_list.indexOf(result), 1);
+       //result = {users_list: result};
+       res.status(200).end();
+   }
+})
+
 function addUser(user){
    users['users_list'].push(user);
 }
@@ -53,6 +79,10 @@ function findUserById(id) {
 
 const findUserByName = (name) => { 
    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job, usersList) => {
+   return usersList.filter( (user) => user['job'] === job);
 }
 
 const users = { 
@@ -87,6 +117,11 @@ const users = {
          id: 'zap555', 
          name: 'Dennis',
          job: 'Bartender',
+      },
+      {
+         id: 'zap555', 
+         name: 'Dennis',
+         job: 'Janitor',
       }
    ]
 }
